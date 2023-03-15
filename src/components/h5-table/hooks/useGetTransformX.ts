8e2bc07fe34204c93469a8ccbca2ef5af8c09b25
liuqiao <1264256716@qq.com>
2023-03-15 19:19:02 +0800

    优化滚动问题

--- src/components/h5-table/hooks/useGetTransformX.ts
+++ src/components/h5-table/hooks/useGetTransformX.ts
@@ -1,41 +1,48 @@
-import useTouchMoveLeftAndRight from './useTouchMoveLeftAndRight'
-import {  ref, type Ref } from 'vue';
-
+import useTouchMoveLeftAndRight from "./useTouchMoveLeftAndRight";
+import { ref, type Ref } from "vue";
 
 //计算左右能移动的 px 位移
-export default function useGetTransformX(target:Ref<HTMLElement|null>,tablewidth:Ref<number>,tableContent:Ref<number>){
-    
-    const previousX = ref<number>(0)
+export default function useGetTransformX(
+  target: Ref<HTMLElement | null>,
+  tablewidth: Ref<number>,
+  tableContent: Ref<number>,
+  stopPropagation: boolean = true
+) {
+  const previousX = ref<number>(0);
 
-    const transformX = ref<number>(0)
+  const transformX = ref<number>(0);
 
-    const touchend = ()=>{
-        previousX.value = transformX.value
-        distanX.value = 0
-        distanY.value = 0
+  const touchend = () => {
+    previousX.value = transformX.value;
+    distanX.value = 0;
+    distanY.value = 0;
+  };
+
+  const touchmove = (event: TouchEvent, direction: string) => {
+    if (direction) {
+      event.cancelable && event.preventDefault();
+      const max = tableContent.value - tablewidth.value;
+      console.log(5555, tableContent.value, tablewidth.value);
+
+      // 只有表格内容超过了 表格外部 才让拖动
+      if (max > 0) {
+        const temp = Math.min(previousX.value + distanX.value, 0);
+        const res = Math.max(-max, temp);
+        transformX.value = res;
+      }
     }
 
-    
-    const touchmove = (event:TouchEvent,direction:string)=>{
-        if(direction){
-            event.cancelable&&event.preventDefault();
-            const max = tableContent.value-tablewidth.value
-            console.log(5555,tableContent.value,tablewidth.value);
-            
-            // 只有表格内容超过了 表格外部 才让拖动
-            if(max>0){
-                const temp = Math.min(previousX.value + distanX.value,0)
-                const res =  Math.max(-max,temp)
-                transformX.value = res
-            }
-        }
+    if (stopPropagation) {
+      if (target.value?.scrollTop !== 0) {
+        event.stopPropagation();
+      }
     }
+  };
 
-    const [distanX,distanY] = useTouchMoveLeftAndRight(target,{
-        touchmove,touchend
-    })
+  const [distanX, distanY] = useTouchMoveLeftAndRight(target, {
+    touchmove,
+    touchend,
+  });
 
-    return [transformX,distanX,distanY]
-
-
+  return [transformX, distanX, distanY];
 }
