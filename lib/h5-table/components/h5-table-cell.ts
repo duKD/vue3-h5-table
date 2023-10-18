87ea3f26a375ca7501c91db93e589d76df559622
liuqiao <1264256716@qq.com>
2023-10-18 15:07:24 +0800

    perf: 优化h tht-table-cell 为函数组件 减少渲染时间

--- lib/h5-table/components/h5-table-cell.ts
+++ lib/h5-table/components/h5-table-cell.ts
@@ -1,20 +1,24 @@
-import { defineComponent, h } from "vue";
+import { h, type Slots, type FunctionalComponent } from "vue";
 
-export default defineComponent({
-  name: "H5TableCell",
-  props: ["render", "dataItem", "dataValue", "slotKey", "slots"],
-  setup(props) {
-    return () => {
-      // 插槽优先级最高
-      if (props.slots && props.slotKey && props.slots[props.slotKey]) {
-        return props.slots[props.slotKey](props.dataItem);
-      }
-      // 自定义 render
-      if (props.render) {
-        return props.render(h, props.dataItem);
-      }
-      // 默认value
-      return h("div", { class: "cell", innerHTML: props.dataValue });
-    };
-  },
-});
+type PropsType = {
+  render?: Function;
+  dataItem?: any;
+  dataValue: string;
+  slotKey: string | undefined;
+  slots: Slots;
+};
+
+const H5TableCell: FunctionalComponent<PropsType> = (props) => {
+  // 插槽优先级最高
+  if (props.slots && props.slotKey && props.slots[props.slotKey]) {
+    return props.slots[props.slotKey]!(props.dataItem);
+  }
+  // 自定义 render
+  if (props.render) {
+    return props.render(h, props.dataItem);
+  }
+  // 默认value
+  return h("div", { class: "cell", innerHTML: props.dataValue });
+};
+
+export default H5TableCell;
